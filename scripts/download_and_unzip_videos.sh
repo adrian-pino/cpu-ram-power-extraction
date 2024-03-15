bbae44b1a046057700abf8e8a86d4e78d17de768
Adrian Pino <48448195+adrian-pino@users.noreply.github.com>
2024-03-15 19:06:38 +0100

    Update download_and_unzip_videos.sh

--- scripts/download_and_unzip_videos.sh
+++ scripts/download_and_unzip_videos.sh
@@ -3,11 +3,18 @@
 # Go to videos directory
 cd ../videos
 
-# Download
-wget https://download.blender.org/demo/movies/BBB/bbb_sunflower_2160p_30fps_normal.mp4.zip  
-wget https://download.blender.org/demo/movies/BBB/bbb_sunflower_2160p_60fps_normal.mp4.zip  
-wget https://download.blender.org/demo/movies/BBB/bbb_sunflower_1080p_60fps_normal.mp4.zip  
+# Download BBB film as a testing video
+# Old Film release
+# wget https://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_1080p_h264.mov
+
+# New Film release
+# 30 fps 
+wget https://download.blender.org/demo/movies/BBB/bbb_sunflower_2160p_30fps_normal.mp4.zip
 wget https://download.blender.org/demo/movies/BBB/bbb_sunflower_1080p_30fps_normal.mp4.zip 
+
+# 60 fps
+# wget https://download.blender.org/demo/movies/BBB/bbb_sunflower_2160p_60fps_normal.mp4.zip  
+# wget https://download.blender.org/demo/movies/BBB/bbb_sunflower_1080p_60fps_normal.mp4.zip  
 
 # Unzip
 unzip '*.zip'
