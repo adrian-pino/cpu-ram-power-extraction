8867d4cc807d459c5f23d5d562eb0131efae2d22
Adrian Pino <adrian.pino@i2cat.net>
2024-03-18 12:02:30 +0000

    Update download videos script. Avoid 60fps download

--- scripts/download_and_unzip_videos.sh
+++ scripts/download_and_unzip_videos.sh
@@ -13,8 +13,8 @@ wget https://download.blender.org/demo/movies/BBB/bbb_sunflower_2160p_30fps_norm
 wget https://download.blender.org/demo/movies/BBB/bbb_sunflower_1080p_30fps_normal.mp4.zip 
 
 # 60 fps
-wget https://download.blender.org/demo/movies/BBB/bbb_sunflower_2160p_60fps_normal.mp4.zip  
-wget https://download.blender.org/demo/movies/BBB/bbb_sunflower_1080p_60fps_normal.mp4.zip  
+# wget https://download.blender.org/demo/movies/BBB/bbb_sunflower_2160p_60fps_normal.mp4.zip  
+# wget https://download.blender.org/demo/movies/BBB/bbb_sunflower_1080p_60fps_normal.mp4.zip  
 
 # Unzip
 unzip '*.zip'
