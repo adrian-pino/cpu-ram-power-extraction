b2c25b619f78b36d5ad2c7e3ca12682bb3fe33d1
Adrian Pino <adrian.pino@i2cat.net>
2024-03-18 11:59:51 +0000

    Update naming for the histograms

--- scripts/plot_cpu_ram.py
+++ scripts/plot_cpu_ram.py
@@ -3,7 +3,6 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import glob
-import psutil
 
 def plot_histograms(log_file_path):
     # Load the log file into a DataFrame
@@ -11,34 +10,34 @@ def plot_histograms(log_file_path):
     df["CPU_Usage"] = 100 - df["CPU_Idle(%)"]
     df["Memory_Used_MB"] = df["Memory_Used(KB)"] / 1024
 
-    base_name = os.path.basename(log_file_path)
-    plot_name_prefix = "plot_" + base_name[:-4]  # removing '.log'
-    base_path = os.path.dirname(log_file_path)
+    # Extract date and time information from the log file name
+    file_name = os.path.basename(log_file_path)
+    datetime_info = file_name.split("_")[4]  # Extracting date and time from the file name
+    date_time = datetime_info.replace("-", "_")  # Replace '-' with '_' for consistency
 
-    # Get the maximum RAM capacity of the device (in bytes)
-    max_ram_capacity_bytes = psutil.virtual_memory().total
-
-    # Convert the maximum RAM capacity to megabytes
-    max_ram_capacity_mb = max_ram_capacity_bytes / (1024 * 1024)
+    # Get the directory where the log file is located
+    log_dir = os.path.dirname(log_file_path)
 
     # Plot CPU Usage Histogram
     plt.figure(figsize=(10, 6))
-    plt.hist(df["CPU_Usage"], bins=8, color='lightcoral', edgecolor='black', range=(0, 100))
+    plt.hist(df["CPU_Usage"], bins=8, color='lightcoral', edgecolor='black')
     plt.title("Histogram of CPU Usage (%)")
     plt.xlabel("CPU Usage (%)")
     plt.ylabel("Frequency")
     plt.grid(axis='y', alpha=0.75)
-    plt.savefig(os.path.join(base_path, f"{plot_name_prefix}_cpu_histogram.png"))
+    cpu_plot_name = os.path.join(log_dir, f"cpu_histogram_{date_time}.png")
+    plt.savefig(cpu_plot_name)
     plt.close()
 
     # Plot RAM Usage Histogram
     plt.figure(figsize=(10, 6))
-    plt.hist(df["Memory_Used_MB"], bins=8, color='skyblue', edgecolor='black', range=(0, max_ram_capacity_mb))
+    plt.hist(df["Memory_Used_MB"], bins=8, color='skyblue', edgecolor='black')
     plt.title("Histogram of RAM Used (MB)")
     plt.xlabel("RAM Used (MB)")
     plt.ylabel("Frequency")
     plt.grid(axis='y', alpha=0.75)
-    plt.savefig(os.path.join(base_path, f"{plot_name_prefix}_ram_histogram.png"))
+    ram_plot_name = os.path.join(log_dir, f"ram_histogram_{date_time}.png")
+    plt.savefig(ram_plot_name)
     plt.close()
 
 if __name__ == "__main__":
