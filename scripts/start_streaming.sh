9030b21903510dd54fe67980cb236e0da678f084
Adrián Pino <adrian.pino@i2cat.net>
2024-05-08 11:21:14 +0200

    Parameterize start_streaming script

--- scripts/start_streaming.sh
+++ scripts/start_streaming.sh
@@ -1,12 +1,19 @@
-#/bin/bash
+#!/bin/bash
 set -x 
+
+# Define the streaming parameters
 STREAMING_TIME=1100
+VIDEO_PATH="../videos"
+VIDEO_NAME="hiv0199_test_und_1080.mp4"
+STREAMING_RECEIVER_IP="192.168.40.114"
+STREAMING_RECEIVER_PORT="8554"
+STREAM_NAME="mystream"
 
-# Debugging purposes; stream only 5 sec
-# ffmpeg -re -i ../videos/hiv0199_test_und_1080.mp4 -c copy -an -t 10 -f rtsp rtsp://192.168.40.114:8554/mystream
+# Debugging purposes; stream only 10 seconds
+# ffmpeg -re -i "$VIDEO_PATH/$VIDEO_NAME" -c copy -an -t 10 -f rtsp "rtsp://$STREAMING_RECEIVER_IP:$STREAMING_RECEIVER_PORT/$STREAM_NAME"
 
-# Stream the hole video on loop (inf)
-# ffmpeg -re -stream_loop -1 -i ../videos/hiv0199_test_und_1080.mp4 -c copy -an -f rtsp rtsp://192.168.40.114:8554/mystream
+# Stream the whole video on loop (infinitely)
+# ffmpeg -re -stream_loop -1 -i "$VIDEO_PATH/$VIDEO_NAME" -c copy -an -f rtsp "rtsp://$STREAMING_RECEIVER_IP:$STREAMING_RECEIVER_PORT/$STREAM_NAME"
 
-# Stream the video the seconds specified in STREAMING_TIME
-ffmpeg -re -stream_loop -1 -i ../videos/hiv0199_test_und_1080.mp4 -c copy -an -t $STREAMING_TIME -f rtsp rtsp://192.168.40.114:8554/mystream
+# Stream the video for the specified duration in STREAMING_TIME
+ffmpeg -re -stream_loop -1 -i "$VIDEO_PATH/$VIDEO_NAME" -c copy -an -t "$STREAMING_TIME" -f rtsp "rtsp://$STREAMING_RECEIVER_IP:$STREAMING_RECEIVER_PORT/$STREAM_NAME"
