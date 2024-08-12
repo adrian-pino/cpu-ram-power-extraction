8fdbd62f19f6ea9401d4ab7303bc2393f2ff5011
Adrian Pino <adrian.pino@i2cat.net>
2024-08-12 13:17:58 +0200

    Add power consumption extraction to the run_experiment logic

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
+# Script to start streaming, monitor CPU/RAM usage, and monitor power consumption.
+
+# Kill any existing instances of the scripts to avoid zombie processes
+pkill -f start_streaming.sh
+pkill -f monitor_cpu_ram.sh
+pkill -f monitor_power.sh
+
 # Start streaming in the background
 ./start_streaming.sh &
 VLC_PID=$!
 
-# Start monitoring in the background
+# Start monitoring CPU/RAM usage in the background
 ./monitor_cpu_ram.sh &
-MONITOR_PID=$!
+MONITOR_CPU_RAM_PID=$!
+
+# Start monitoring power consumption in the background
+./monitor_power_consumption.sh &
+MONITOR_POWER_PID=$!
 
 # Wait for VLC to finish
 wait $VLC_PID
 
-# Once VLC is done, kill the monitoring script
-kill $MONITOR_PID
+# Once VLC is done, kill the monitoring scripts
+kill $MONITOR_CPU_RAM_PID
+kill $MONITOR_POWER_PID
 
 echo ""
 echo "Experiment finished successfully"
@@ -21,3 +33,4 @@ echo ""
 source venv/bin/activate
 ./generate_plots.sh
 echo "Plots generated successfully"
+
